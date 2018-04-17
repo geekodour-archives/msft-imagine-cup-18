81c25425fe2a0facb9ba2e8b7d26c7507d8ffc9e
geekodour <hrishikeshbman@gmail.com>
2018-04-17 06:06:13 +0530

    mmm

--- src/inject/inject_facebook.js
+++ src/inject/inject_facebook.js
@@ -1,20 +1,49 @@
 let post_count = 0;
 let last_pos = 0;
+
+function genButton(buttonText){
+    let button = document.createElement("button");
+    button.innerText = buttonText;
+    button.className = "ibetter_button"
+    return button;
+}
+
+function filterByLinkPosts(post){
+    let a = post.querySelectorAll('a[target="_blank"]')
+    a = Array.prototype.map.call(a,function(aa){
+        return window.decodeURIComponent(aa.search).slice(3).split('&')[0]
+    });
+    if(a.length>0){ return true; } else { return false; }
+}
+
+
 function addButtonsToPosts(){
     let all_posts = document.querySelectorAll('._1dwg._1w_m._q7o');
+    all_posts = Array.prototype.filter.call(all_posts,filterByLinkPosts)
     let total_post_count = all_posts.length;
+
     if(total_post_count>post_count){
-        //post_count = total_post_count;
         all_posts = Array.prototype.slice.call(all_posts,post_count)
-        //all_posts.slice(post_count);
         Array.prototype.forEach.call(all_posts,function(post){
             post_count += 1;
+            post.querySelector('._4r_y').classList.add('ibetter_container')
+
+            let a = post.querySelectorAll('a[target="_blank"]')
+            a = Array.prototype.map.call(a,function(aa){
+                return window.decodeURIComponent(aa.search).slice(3).split('&')[0]
+            });
+            let checkBtn = genButton('Check');
+            checkBtn.onclick = function(){
+                console.log(a[0]);
+            }
+
+
             let buttonDiv = post.querySelector('div');
             let newItem = document.createElement("div");  
-            newItem.innerText = "POOP";
             newItem.className = "ibetter_div";
+            newItem.appendChild(checkBtn)
+            newItem.appendChild(genButton('Similar'))
             post.insertBefore(newItem, buttonDiv);
-            //buttonDiv.insertBefore(newItem, buttonDiv.childNodes[0]);
         })
     } 
 }
@@ -26,14 +55,26 @@ chrome.extension.sendMessage({}, function(response) {
         clearInterval(readyStateCheckInterval);
 
         let all_posts = document.querySelectorAll('._1dwg._1w_m._q7o');
+        all_posts = Array.prototype.filter.call(all_posts,filterByLinkPosts)
         // do it for the initial time
         Array.prototype.forEach.call(all_posts,function(post){
             post_count += 1;
+            post.querySelector('._4r_y').classList.add('ibetter_container')
             let buttonDiv = post.querySelector('div');
             let newItem = document.createElement("div");  
-            newItem.innerText = "POOP";
+
+            let a = post.querySelectorAll('a[target="_blank"]')
+            a = Array.prototype.map.call(a,function(aa){
+                return window.decodeURIComponent(aa.search).slice(3).split('&')[0]
+            });
+            let checkBtn = genButton('Check');
+            checkBtn.onclick = function(){
+                console.log(a[0]);
+            }
+            newItem.appendChild(checkBtn);
+            newItem.appendChild(genButton('Similar'));
+            newItem.className = "ibetter_div";
             post.insertBefore(newItem, buttonDiv);
-            //buttonDiv.insertBefore(newItem, buttonDiv.childNodes[0]);
         })
 
         window.addEventListener('scroll', function(e) {
@@ -42,12 +83,7 @@ chrome.extension.sendMessage({}, function(response) {
                 addButtonsToPosts()
             }
             last_pos = pos;
-            //console.log(total_post_count)
         });
-
-        //console.log("Happening fater page load");
-        //console.log("Happening fater page load");
-        //console.log("FACEBOOOOOKK  ONLY");
 	}
 	}, 1);
 });
