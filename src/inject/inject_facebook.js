f0493ff52ac9f6350605ac1ba3258ecbf838ffad
geekodour <hrishikeshbman@gmail.com>
2018-04-19 00:22:00 +0530

    jkhjh# Please enter the commit message for your changes. Lines starting

--- src/inject/inject_facebook.js
+++ src/inject/inject_facebook.js
@@ -8,6 +8,21 @@ function genButton(buttonText){
     return button;
 }
 
+function genListOfLinks(list){
+    let div = document.createElement("div");
+    let ul = document.createElement("ul");
+    list.forEach(function(l){
+        let li = document.createElement("li");
+        let a = document.createElement("a");
+        a.href = l.url;
+        a.innerText = l.url;
+        li.appendChild(a);
+        ul.appendChild(li);
+    })
+    div.appendChild(ul);
+    return div;
+}
+
 function filterByLinkPosts(post){
     let a = post.querySelectorAll('a[target="_blank"]')
     a = Array.prototype.map.call(a,function(aa){
@@ -33,29 +48,63 @@ function addButtonsToPosts(){
                 return window.decodeURIComponent(aa.search).slice(3).split('&')[0]
             });
             let checkBtn = genButton('Check');
+            let simBtn = genButton('Similar');
+            let postInfo = {}
+
+            simBtn.onclick = function(){
+                if(postInfo.similar_results){
+                    let similar_results = postInfo.similar_results
+                    link_div = genListOfLinks(similar_results)
+                    post.insertBefore(link_div, buttonDiv);
+                    console.log(similar_results)
+                } else {
+                    fetch('https://ibetter.herokuapp.com/api/detect/?url='+a[0])
+                      .then(function(response) {
+                        return response.json();
+                      })
+                      .then(function(myJson) {
+                        postInfo = myJson;
+                        let similar_results = postInfo.similar_results
+
+                        link_div = genListOfLinks(similar_results)
+                        post.insertBefore(link_div, buttonDiv);
+                        console.log(similar_results)
+                      });
+                }
+            }
+
             checkBtn.onclick = function(){
-                fetch('https://ibetter.herokuapp.com/api/detect/?url='+a[0])
-                  .then(function(response) {
-                    return response.json();
-                  })
-                  .then(function(myJson) {
-                    let pred = +myJson.our_prediction.toFixed(1);
-                    if(pred>60){
+                if(postInfo.similar_results){
+                    let pred = +postInfo.our_prediction.toFixed(1);
+                    if(pred>50){
                         checkBtn.classList.add('real');
                     } else {
                         checkBtn.classList.add('fake');
                     }
                     checkBtn.innerText = pred+"%";
-                    console.log(myJson);
-                  });
+                } else {
+                    fetch('https://ibetter.herokuapp.com/api/detect/?url='+a[0])
+                      .then(function(response) {
+                        return response.json();
+                      })
+                      .then(function(myJson) {
+                        postInfo = myJson;
+                        let pred = +postInfo.our_prediction.toFixed(1);
+                        if(pred>50){
+                            checkBtn.classList.add('real');
+                        } else {
+                            checkBtn.classList.add('fake');
+                        }
+                        checkBtn.innerText = pred+"%";
+                      });
+                }
             }
-
 
             let buttonDiv = post.querySelector('div');
             let newItem = document.createElement("div");  
             newItem.className = "ibetter_div";
             newItem.appendChild(checkBtn)
-            newItem.appendChild(genButton('Similar'))
+            newItem.appendChild(simBtn)
             post.insertBefore(newItem, buttonDiv);
         })
     } 
@@ -81,24 +130,60 @@ chrome.extension.sendMessage({}, function(response) {
                 return window.decodeURIComponent(aa.search).slice(3).split('&')[0]
             });
             let checkBtn = genButton('Check');
+            let simBtn = genButton('Similar');
+            let postInfo = {}
+
+            simBtn.onclick = function(){
+                if(postInfo.similar_results){
+                    let similar_results = postInfo.similar_results
+                    link_div = genListOfLinks(similar_results)
+                    post.insertBefore(link_div, buttonDiv);
+                    console.log(similar_results)
+                } else {
+                    fetch('https://ibetter.herokuapp.com/api/detect/?url='+a[0])
+                      .then(function(response) {
+                        return response.json();
+                      })
+                      .then(function(myJson) {
+                        postInfo = myJson;
+                        let similar_results = postInfo.similar_results
+                        link_div = genListOfLinks(similar_results)
+                        post.insertBefore(link_div, buttonDiv);
+                        console.log(similar_results)
+                      });
+                }
+            }
+
             checkBtn.onclick = function(){
-                fetch('https://ibetter.herokuapp.com/api/detect/?url='+a[0])
-                  .then(function(response) {
-                    return response.json();
-                  })
-                  .then(function(myJson) {
-                    let pred = +myJson.our_prediction.toFixed(1);
-                    if(pred>60){
+                if(postInfo.similar_results){
+                    let pred = +postInfo.our_prediction.toFixed(1);
+                    if(pred>50){
                         checkBtn.classList.add('real');
                     } else {
                         checkBtn.classList.add('fake');
                     }
                     checkBtn.innerText = pred+"%";
-                    console.log(myJson);
-                  });
+                } else {
+                    fetch('https://ibetter.herokuapp.com/api/detect/?url='+a[0])
+                      .then(function(response) {
+                        return response.json();
+                      })
+                      .then(function(myJson) {
+                        postInfo = myJson;
+                        let pred = +postInfo.our_prediction.toFixed(1);
+                        if(pred>50){
+                            checkBtn.classList.add('real');
+                        } else {
+                            checkBtn.classList.add('fake');
+                        }
+                        checkBtn.innerText = pred+"%";
+                      });
+                }
             }
+
+
             newItem.appendChild(checkBtn);
-            newItem.appendChild(genButton('Similar'));
+            newItem.appendChild(simBtn)
             newItem.className = "ibetter_div";
             post.insertBefore(newItem, buttonDiv);
         })
